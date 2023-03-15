753cce234cf5be9f28ed0308be8ec25212ac4f71
PedroNevesHespanhol <88465619+PedroNevesHespanhol@users.noreply.github.com>
2023-03-14 21:30:10 -0300

    TrabalhandoComArray

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -1,49 +1,62 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Cadastro() {
+    const [input, setInput] = useState('');
+    const [tarefas, setTarefas] = useState([
+        "Pagar conta de luz,",
+        "Estudar front-end",
+        "Enviar a tarefa"
+    ]);
     const [nome, setNome] = useState('');
-    const [email, setEmail] = useState('');
-    const [idade, setIdade] = useState(0);
+    
+    useEffect(() => {
 
-    const [nomeTela, setNomeTela] = useState('');
-    const [emailTela, setEmailTela] = useState('');
-    const [idadeTela, setIdadeTela] = useState(0);
+        const nomeNovo = prompt("Qual é o seu nome?");
+        setNome(nomeNovo);
+        localStorage.setItem("nome", nomeNovo);
+        
+    }, []);
+    
+    //Pega o que esta no localStorage
+    const tarefasStorage = localStorage.getItem('@tarefa');
 
-    const handleSubmit = (e) => {
+    //Verifica se ja possui itens no localStorage e se possui passa esses valores
+    useEffect(() => {
+        
+        if(tarefasStorage){
+            setTarefas(JSON.parse(tarefasStorage));
+        }
+
+    }, []);
+
+    useEffect(() => {
+        //@tarefa e a key de id do localStorage, stringify converte o array para JSON, pois localStorage so aceita JSON
+        localStorage.setItem('@tarefa', JSON.stringify(tarefas));
+    }, [tarefas]);
+
+    function handleRegistro(e) {
         e.preventDefault();
 
-        setNomeTela(nome);
-        setNome("");
-
-        setEmailTela(email);
-        setEmail("");
-
-        setIdadeTela(idade);
-        setIdade("");
+        //spread operator(...), faz uma copia do array e add o proximo item a frente
+        setTarefas([...tarefas, input]);
+        setInput('');//zera o input novamente
     }
 
-    const handleClick = () => {
-
-    }
   return (
     <>
-        <form onSubmit={handleSubmit}>
-            <p>Nome:</p>
-            <input type="text" placeholder="Digite seu nome" onChange={(e) => setNome(e.target.value)}/>
-            <p>Email:</p>
-            <input type="email" placeholder="Digite seu email" onChange={(e) => setEmail(e.target.value)}/>
-            <p>Idade:</p>
-            <input type="number" placeholder="Complete com sua idade" onChange={(e) => setIdade(e.target.value)}/>
-            <br/>
-            <button type="submit" onClick={handleClick}>Registro</button>
-        </form>
-        <div>
-            <p>Bem vindo {nomeTela}</p>
-            <p>Email: {emailTela}</p>
-            <p>Idade: {idadeTela}</p>
-
-        </div>
+        <h1>{ nome }, sua lista de tarefas</h1>
+        <form onSubmit={handleRegistro}>
+            {/* <span>Sua lista de tarefas!</span><br/> */}
+            <input type="text" placeholder="Digite uma tarefa" value={ input } onChange={(e) => setInput(e.target.value)}/><br/>
+            <button type="submit">Registro</button>
+        </form><br/>
+        <ul>
+            {tarefas.map( tarefa => (
+                //sempre que utilizar um array necessita ter uma key
+                <li key={tarefa}>{tarefa}</li>
+            ))}
+        </ul>
     </>
   );
 }
